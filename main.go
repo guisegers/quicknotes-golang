ff2b3ef37982aae48c7468bd326f8a4fd5c840ee
bitconho <guisegers@gmail.com>
2026-02-26 03:37:51 -0300

    Atualização CSS

--- main.go
+++ main.go
@@ -3,17 +3,74 @@ package main
 import (
 	"fmt"
 	"net/http"
-
-	"quicknotes/controller"
+	"text/template"
 )
+
+// NoteHandler implements http.Handler for the note list (root path).
+type NoteHandler struct{}
+
+func noteList(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
+	files := []string{
+		"views/templates/base.html",
+		"views/templates/pages/home.html",
+	}
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	t, err := template.ParseFiles(files...)
+	if err != nil {
+		http.Error(w, "Aconteceu um erro ao executar essa página", http.StatusInternalServerError)
+		return
+	}
+	t.ExecuteTemplate(w, "base", nil)
+}
+
+func noteView(w http.ResponseWriter, r *http.Request) {
+	files := []string{
+		"views/templates/base.html",
+		"views/templates/pages/home.html",
+		"views/templates/pages/note-view.html",
+	}
+	id := r.URL.Query().Get("id")
+	t, err := template.ParseFiles(files...)
+	if err != nil {
+		http.Error(w, "Aconteceu um erro ao executar essa página", http.StatusInternalServerError)
+		return
+	}
+	t.ExecuteTemplate(w, "base", id)
+}
+
+func noteNew(w http.ResponseWriter, r *http.Request) {
+	files := []string{
+		"views/templates/base.html",
+		"views/templates/pages/home.html",
+		"views/templates/pages/note-new.html",
+	}
+	// if r.Method != http.MethodPost {
+	// 	w.Header().Set("Allow", http.MethodPost)
+	// 	http.Error(w, http.StatusText(http.StatusMethodNotAllowed), http.StatusMethodNotAllowed)
+	// 	return
+	// }
+	t, err := template.ParseFiles(files...)
+	if err != nil {
+		http.Error(w, "Aconteceu um erro ao executar essa página", http.StatusInternalServerError)
+		return
+	}
+	t.ExecuteTemplate(w, "base", nil)
+}
 
 func main() {
 	fmt.Println("Servidor rodando na porta 8080")
 	mux := http.NewServeMux()
 
-	mux.HandleFunc("/", controller.NoteList)
-	mux.HandleFunc("/note/view", controller.NoteView)
-	mux.HandleFunc("/note/create", controller.NoteCreate)
+	staticHandler := http.FileServer(http.Dir("views/static"))
+	mux.Handle("/static/", http.StripPrefix("/static/", staticHandler))
+
+	mux.HandleFunc("/", noteList)
+	mux.HandleFunc("/note/view", noteView)
+	mux.HandleFunc("/note/new", noteNew)
 
 	http.ListenAndServe(":8080", mux)
 }
